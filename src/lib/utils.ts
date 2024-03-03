35eb244dd972d46ed9a329ce5921573688202255
Matteo Martellini <git@mercxry.me>
2024-03-04 00:00:54 +0100

    fix: warning

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,8 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
 
-function enumFromString<T>(enm: { [s: string]: T }, value: string): T | undefined {
-    return (Object.values(enm) as unknown as string[]).includes(value)
-        ? value as unknown as T
-        : undefined;
-}
+/* function enumFromString<T>(enm: { [s: string]: T }, value: string): T | undefined { */
+/*     return (Object.values(enm) as unknown as string[]).includes(value) */
+/*         ? value as unknown as T */
+/*         : undefined; */
+/* } */
